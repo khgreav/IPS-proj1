32a8150c6ffa4b998a4526131fbb15d22b4ab922
Karel Hanák <khgreav@gmail.com>
2018-10-11 12:30:55 +0200

    super trihard commit, argument control

---
+++ psed.c
@@ -0,0 +1,11 @@
+/* Karel Hanák (xhanak34), David Sedlák (xsedla1d) */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    if (argc % 2 == 0 && argc >= 4) { //argument control
+
+    } else {
+        printf("Invalid number of arguments.\n");
+    }
+}
